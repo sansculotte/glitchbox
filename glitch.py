9f9580201c0ba2f5950e32ba199b120b75c42e9b
ub <u@sansculotte.net>
2015-01-28 22:50:35 +0100

    fixed a bug where height and width were mixed up

--- glitch.py
+++ glitch.py
@@ -11,30 +11,40 @@ class Glitcher:
     Inject, distort, disturb, intersect, explode an image file with various
     methods of destruction
     """
-    def __init__(self, img=None, filename=None):
+    def __init__(self, args):
         """ Run the glitcher """
         self.bitmap = None
-        if img:
-            self.bitmap = img
-        if filename:
-            self.bitmap = cv2.imread( filename )
+        if args.imagefile:
+            self.bitmap = cv2.imread( args.imagefile )
+        try:
+            self.margin = args.margin
+        except:
+            self.margin = None
+            
 
     def glitch(self, amount):
         """ Run the glitcher """
         if not self.bitmap.any():
             raise Exception('No image loaded')
-        self.width, self.height, self.colors = self.bitmap.shape
+        self.height, self.width, self.colors = self.bitmap.shape
         self.line_glitch( amount )
         self.block_glitch( amount )
 
+
     def line_glitch(self, amount):
+        """
+        Move lines in x-direction 
+        """
         for i in range(0, amount):
             line = random.randint( 0, self.height-1 )
             shift = random.randint( 0, amount ) - amount / 2
             self.bitmap[ line ] = np.roll( self.bitmap[ line ], shift, 0 )
 
     def block_glitch(self, amount):
-        margin = ( self.width/10, self.height/10 )
+        """
+        Copy blocks around
+        """
+        margin = ( self.width/10, self.height/10 ) if self.margin is None else ( self.margin, self.margin )
         for i in range(0, amount):
             block_x = (random.randint( 0, self.width-margin[0] ), random.randint( 1, margin[0] ))
             block_y = (random.randint( 0, self.height-margin[1] ), random.randint( 1, margin[1] ))
@@ -63,10 +73,12 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--outfile', dest='outfile', action='store', default='output.png',
         help='image file to write output to')
     parser.add_argument('-a', '--amount', dest='amount', type=int, action='store', default='9',
-        help='image file to write output to')
+        help='fuckup factor, the bigger the more destroyed')
+    parser.add_argument('-m', '--margin', dest='margin', type=int, action='store',
+        help='block size limit, default to 10% of image')
     args = parser.parse_args()
 
-    glitcher = Glitcher( filename=args.imagefile )
+    glitcher = Glitcher( args )
     try:
         glitcher.glitch( args.amount )
     except Exception, e:
