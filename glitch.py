c203e75d3f7cc43d08969d471173759139f6233b
ub <u@sansculotte.net>
2015-01-28 22:00:18 +0100

    block glitch

--- glitch.py
+++ glitch.py
@@ -23,12 +23,34 @@ class Glitcher:
         """ Run the glitcher """
         if not self.bitmap.any():
             raise Exception('No image loaded')
-        width, height, colors = self.bitmap.shape
+        self.width, self.height, self.colors = self.bitmap.shape
+        self.line_glitch( amount )
+        self.block_glitch( amount )
 
+    def line_glitch(self, amount):
         for i in range(0, amount):
-            line = random.randint( 0, height-1 )
-            shift = random.randint( 0, amount )
+            line = random.randint( 0, self.height-1 )
+            shift = random.randint( 0, amount ) - amount / 2
             self.bitmap[ line ] = np.roll( self.bitmap[ line ], shift, 0 )
+
+    def block_glitch(self, amount):
+        margin = ( self.width/10, self.height/10 )
+        for i in range(0, amount):
+            block_x = (random.randint( 0, self.width-margin[0] ), random.randint( 1, margin[0] ))
+            block_y = (random.randint( 0, self.height-margin[1] ), random.randint( 1, margin[1] ))
+            x = max( 0, block_x[0] + random.randint(0, amount) - amount/2 )
+            y = max( 0, block_y[0] + random.randint(0, amount) - amount/2 )
+            while x+block_x[1] < self.width or random.randint(0, amount) > 0:
+                try:
+                    if random.randint(0,10)>5:
+                        self.bitmap[ y:y+block_y[1], x:x+block_x[1] ] = self.bitmap[ block_y[0]:sum(block_y), block_x[0]:sum(block_x) ]
+                    else:
+                        self.bitmap[ x:x+block_x[1], y:y+block_y[1] ] = self.bitmap[ block_y[0]:sum(block_y), block_x[0]:sum(block_x) ]
+                except:
+                    pass
+                x += block_x[1] + random.randint(0, 2) - 1
+                y += random.randint(0, 2) - 1
+
 
     def save(self, outfile):
         """ Save the scrambled files """
