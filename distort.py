e1a3ef23c45e6595299c5a35dc5db73cd495a7f6
ub <u@sansculotte.net>
2016-05-03 00:32:02 +0200

    emerge move to audiopack library

--- distort.py
+++ distort.py
@@ -3,73 +3,18 @@ import os
 import sys
 import argparse
 import cv2
+#from multiprocessing import Pool
 import numpy as np
-import scipy.io.wavfile as wavfile
+from audiopack import loadwav, audio_chunks
 
 """
-Distort image(s) with a soundwave.
-TODO: multichannel distortion
-      the problem here is, that just dividing by number of channels
-      and summing it up, does not add up to the correct brightness
-      and everything gets horribly slow
-      distorting the same data twice looks interesting
+Distort image(s) horizontally by a soundwave.
 """
 
-def pcm2float(sig, dtype='float64'):
-    """Convert PCM signal to floating point with a range from -1 to 1.
-    Use dtype='float32' for single precision.
-    Parameters
-    ----------
-    sig : array_like
-        Input array, must have integral type.
-    dtype : data type, optional
-        Desired (floating point) data type.
-    Returns
-    -------
-    numpy.ndarray
-        Normalized floating point data.
-    See Also
-    --------
-    float2pcm, dtype
-    """
-    sig = np.asarray(sig)
-    if sig.dtype.kind not in 'iu':
-        raise TypeError("'sig' must be an array of integers")
-    dtype = np.dtype(dtype)
-    if dtype.kind != 'f':
-        raise TypeError("'dtype' must be a floating point type")
 
-    i = np.iinfo(sig.dtype)
-    abs_max = 2 ** (i.bits - 1)
-    offset = i.min + abs_max
-    return (sig.astype(dtype) - offset) / abs_max
-
-
-def load_soundfile(args):
-    length = {}
-    rate, data = wavfile.read(args.soundfile)
-    normalized = pcm2float(data, 'float64')
-    print "Samplerate: %d"%rate
-    length['samples'] = len(normalized)
-    length['seconds'] = length['samples']/float(rate)
-    try:
-        channels = data.shape[1]
-        print "Channels: %d"%(channels)
-    except IndexError:
-        channels = 1
-        print "Channels: 1"
-    print "Length: %d samples, %f seconds"%(length['samples'], length['seconds'])
-    return {'rate':rate, 'length':length, 'channels':channels}, normalized
-
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
-
-
-def save_frame(args, number, frame):
-    cv2.imwrite(os.path.join(args.outdir, "frame_%06d.png"%number), frame)
+def save_frame(outdir, number, frame):
+    print "writing frame %06d"%number
+    cv2.imwrite(os.path.join(outdir, "frame_%06d.png"%number), frame)
 
 
 def process_multichannel(frame, block, amount, height, blocksize, channels):
@@ -103,16 +48,19 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--fps', dest='fps', type=int, action='store', default=25,
         help='video framerate'
     )
-    parser.add_argument('-a', '--amount', dest='amount', type=int, action='store', default='9',
+    parser.add_argument('-a', '--amount', dest='amount', type=int, action='store', default=9,
         help='fuckup factor, the bigger the more destroyed'
     )
-    parser.add_argument('-t', '--tremble', dest='tremble', type=int, action='store',
-        help='additional randomness'
+    parser.add_argument('--start-frame', dest='start_frame', type=int, action='store', default=1,
+        help='frame to start from, starting at 1'
+    )
+    parser.add_argument('--length', dest='length', type=int, action='store',
+        help='number of frames to glitch'
     )
     args = parser.parse_args()
 
     try:
-        meta, data = load_soundfile(args)
+        meta, data = loadwav(args.soundfile)
     except Exception, e:
         print "Problem with the Soundfile"
         print e
@@ -133,20 +81,26 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     height, width, colors = bitmap.shape
-    blocksize             = meta['rate'] // args.fps
-    blocks                = meta['length']['samples'] // blocksize
+    blocksize             = meta.rate // args.fps
+    blocks                = meta.samples // blocksize
 
-    for n, b in enumerate(chunks(data, blocksize)):
-        if n > 0 and len(imagefiles) > 1:
-            bitmap = cv2.imread(imagefiles[n % len(imagefiles)])
+    for n, b in enumerate(audio_chunks(data, blocksize), 1):
+
+#        if os.path.exists(os.path.join(args.outdir, "frame_%06d.png"%n)):
+#            continue
+        if args.length is not None and n-args.start_frame > args.length:
+            continue
+        if n < args.start_frame or n > blocks:
+            continue
+        elif len(imagefiles) > 1:
+            bitmap = cv2.imread(imagefiles[n-1 % len(imagefiles)])
+
         frame = bitmap.copy()
-        if len(b) < blocksize:
-            b = np.lib.pad(b, ((blocksize-len(b)) // 2), 'constant')
-        if args.multichannel and meta['channels'] > 1:
-            frame = process_multichannel(frame, b, args.amount, height, blocksize, meta['channels'])
+        if args.multichannel and meta.channels > 1:
+            frame = process_multichannel(frame, b, args.amount, height, blocksize, meta.channels)
         else:
-            if meta['channels'] > 1:
+            if meta.channels > 1:
                 b = b.T[0]
             frame = process(frame, b, args.amount, height, blocksize)
 
-        save_frame(args, n, frame)
+        save_frame(args.outdir, n, frame)
--- emerge.py
+++ emerge.py
@@ -1,72 +1,15 @@
 #!/usr/bin/env python
+
 import os
 import sys
 import argparse
-from collections import namedtuple
 import cv2
 import numpy as np
-import scipy.io.wavfile as wavfile
+from audiopack import loadwav, audio_chunks
 
 """
-Distort image(s) with a soundwave.
-TODO: multichannel distortion
-      the problem here is, that just dividing by number of channels
-      and summing it up, does not add up to the correct brightness
-      and everything gets horribly slow
-      distorting the same data twice looks interesting
+Emerge Image from junk-noise backdrop
 """
-
-def pcm2float(sig, dtype='float64'):
-    """Convert PCM signal to floating point with a range from -1 to 1.
-    Use dtype='float32' for single precision.
-    Parameters
-    ----------
-    sig : array_like
-        Input array, must have integral type.
-    dtype : data type, optional
-        Desired (floating point) data type.
-    Returns
-    -------
-    numpy.ndarray
-        Normalized floating point data.
-    See Also
-    --------
-    float2pcm, dtype
-    """
-    sig = np.asarray(sig)
-    if sig.dtype.kind not in 'iu':
-        raise TypeError("'sig' must be an array of integers")
-    dtype = np.dtype(dtype)
-    if dtype.kind != 'f':
-        raise TypeError("'dtype' must be a floating point type")
-
-    i = np.iinfo(sig.dtype)
-    abs_max = 2 ** (i.bits - 1)
-    offset = i.min + abs_max
-    return (sig.astype(dtype) - offset) / abs_max
-
-
-def load_soundfile(args):
-    rate, data = wavfile.read(args.soundfile)
-    normalized = pcm2float(data, 'float64')
-    print "Samplerate: %d"%rate
-    samples = len(normalized)
-    length  = samples/float(rate)
-    try:
-        channels = data.shape[1]
-        print "Channels: %d"%(channels)
-    except IndexError:
-        channels = 1
-        print "Channels: 1"
-    print "Length: %d samples, %f seconds"%(samples, length)
-    meta = namedtuple('meta', ['rate', 'samples', 'length', 'channels'])
-    return meta(rate, samples, length, channels), normalized
-
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
 
 
 def save_frame(args, number, frame):
@@ -85,12 +28,12 @@ def junkline(junkchunk):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Image Scrambler')
+    parser = argparse.ArgumentParser(description='Emerge Image from Noise backdrop')
     parser.add_argument('imagefile', metavar='imagefile', type=str,
         help='image or directory with images to be scrambled'
     )
     parser.add_argument('-o', '--outdir', dest='outdir', action='store', default='/tmp',
-        help='directory to write frame images to'
+        help='directory to write frames to'
     )
     parser.add_argument('-s', '--soundfile', dest='soundfile', action='store',
         help='soundfile'
@@ -107,14 +50,24 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--threshold', dest='threshold', type=float, action='store', default='0.1',
         help='where to split noise and signal'
     )
+    parser.add_argument('-O', '--offset', dest='offset', type=int, action='store', default='0',
+        help='where to start scraping the junkyard'
+    )
+    parser.add_argument('--start-frame', dest='start_frame', type=int, action='store', default=1,
+        help='frame to start from, starting at 1'
+    )
+    parser.add_argument('--length', dest='length', type=int, action='store',
+        help='number of frames to glitch'
+    )
     args = parser.parse_args()
 
     try:
-        meta, data = load_soundfile(args)
+        meta, data = loadwav(args.soundfile)
     except Exception, e:
         print "Problem with the Soundfile"
         print e
         sys.exit(-1)
+    print "audiofile loaded"
 
     if os.path.isdir(args.imagefile):
         imagefiles = sorted([
@@ -129,26 +82,34 @@ if __name__ == '__main__':
     if bitmap is None:
         print "Problem loading the first Imagefile"
         sys.exit(-1)
+    print "image loaded"
 
     junk = np.fromfile(args.junkfile, dtype='u1')
+    np.roll(junk, args.offset)
+    print "junk loaded"
 
     height, width, colors = bitmap.shape
     blocksize             = meta.rate // args.fps
     blocks                = meta.samples // blocksize
 
     juncnt = 0
-    for n, b in enumerate(chunks(data, blocksize)):
-        if n > 0 and len(imagefiles) > 1:
-            bitmap = cv2.imread(imagefiles[n % len(imagefiles)])
+    for n, b in enumerate(audio_chunks(data.T[0], blocksize), 1):
+        #if os.path.exists(os.path.join(args.outdir, "frame_%06d.png"%n)):
+        #    continue
+        if args.length is not None and n-args.start_frame > args.length:
+            continue
+        if n < args.start_frame or n > blocks:
+            continue
+        elif len(imagefiles) > 1:
+            bitmap = cv2.imread(imagefiles[n-1 % len(imagefiles)])
+
         frame = bitmap.copy()
-        if len(b) < blocksize:
-            b = np.lib.pad(b, ((blocksize-len(b)) // 2), 'constant')
-        shift = np.interp(np.arange(height), np.arange(blocksize), b.T[0])
+        shift = np.interp(np.arange(height), np.arange(blocksize), b)
         for line in xrange(height):
             if shift[line] * shift[line] > args.threshold:
                 frame[line] = frame[line]
             else:
                 frame[line] = junkline(junk[juncnt:juncnt+width])
                 juncnt = (juncnt + width) % len(junk)
-
+        print n
         save_frame(args, n, frame)
